aea4ab375baa3561433b7258ba167211ca6de0c8
Martins <victorcostamartins0@gmail.com>
2025-01-16 11:21:19 -0300

    Single File Component - SFC

---
+++ hello-world/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <!--CHAMANDO O ELEMENTO-->
+      <!--OU PODEMOS USAR DA FORMA SIMPLIFICANDO -->
+      <!-- <TheHeader /> -->
+    <TheHeader></TheHeader> 
+
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+  // IMPORTAÇÃO DO ARQUIVO
+  //  PASSANDO O NOME "TheHeader"  E O CAMINHA "'./components/TheHeader.vue'"
+import TheHeader from './components/TheHeader.vue'
+
+export default {
+  name: 'App',
+  
+  // o "components" vai declarar os componetes q vamos usar no elemente externo
+  components: {
+    HelloWorld,
+
+    // PASSAREMOS O ELEMENTO PRA ASSIM SEM SER "EXPORTADO" A PG
+    TheHeader
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ hello-world/src/components/TheHeader.vue
@@ -0,0 +1,26 @@
+<!--
+    SINGLE FILE COMPONENT - SFC
+    "componete este onde agrupa os arquivo de forma especifica - 'o template - a logica e o estilo' "
+-->
+
+<!--TEMPLATE - HTML -->
+<template>
+    <header class="header">
+        Header
+    </header>
+</template>
+
+<!--LOGICA - JS -->
+<script>
+    export default {
+
+    }
+</script>
+
+<!--ESTILO - CSS -->
+<style>
+    .header {
+        font-size: 1rem;
+        background-color: aqua;
+    }
+</style>
